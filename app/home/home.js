68c1f436eb2bd670bb8a5ecfa9d401fe355d2a9f
irthos <irth03@gmail.com>
2014-12-06 19:21:17 -0800

    initial wiring up of requirements with note section

--- app/home/home.js
+++ app/home/home.js
@@ -10,7 +10,7 @@
    */
   angular
     .module('home', [
-      'ui.router'
+      'ui.router', 'firebase'
     ]);
 
   angular
--- dist/js/home/home-controller.js
+++ dist/js/home/home-controller.js
@@ -12,37 +12,20 @@
     .module('home')
     .controller('HomeCtrl', HomeCtrl);
 
-  function HomeCtrl() {
-    var vm = this;
+  function HomeCtrl($firebase, Firebase, $scope) {
+    var vm = this,
+      dataURL = 'http://tezt.firebaseio.com/jaapharma',
+      database  = new Firebase(dataURL);
+    vm.data = $firebase(database);
+    vm.sync = vm.data.$asObject();
+    setTimeout(function () {
+      vm.projectRequirements = vm.sync.requirements;
+      console.log(vm.projectRequirements);
+      vm.sync.$bindTo($scope, 'reqs');
+    }, 4000);
+    console.log('vm.sync', vm.sync);
     vm.ctrlName = 'HomeCtrl';
-    vm.projectRequirements = [
-      {name: 'app logo', type: 'image', data:{url: 'string', alt: 'string'}},
-      {name: 'lab code', type: 'string'},
-      {name: 'rep code', type: 'string'},
-      {name: 'zone, region, province', type: 'string'},
-      {name: 'lab logo', type: 'image', data:{url: 'string', alt: 'string'}},
-      {name: 'city list', type: 'array', data:{name: 'string'}},
-      {name: 'municipality list', type: 'array', data:{name: 'string'}},
-      {name: 'doctor list', type: 'array', data:{
-        name: 'string',
-        address:'string',
-        phone:'string',
-        public:'boolean',
-        specialty:'string'}
-      },
-      {name: 'map tracking with date and time', type: 'function', methods: [
-        {name: 'mapTrack', args:['location', 'user'], actions:['gather location data', 'push to server']}]
-      },
-      {name: 'products name list', type: 'array', data:{name: 'string'}},
-      {name: 'products name and form list', type: 'array', data:{name: 'string'}},
-      {name: 'comments and request notes', type: 'array', data:{name: 'string'}},
-      {name: 'number of samples given', type: 'array', data:{name: 'string'}},
-      {name: 'save report and email', type: 'function', methods: [
-        {name: 'sendReport', args:['report', 'email'], actions:['gather report data', 'push to server']}]
-      },
-      {name: 'statistical analytics', type: 'function', methods: [
-        {name: 'sendAnalytics', args:['userActions', 'email'], actions:['gather user actions', 'push to server']}]}
-    ];
+
   }
 
 })();
